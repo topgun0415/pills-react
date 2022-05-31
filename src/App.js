b5d399a571ddfcad4434a155e74236d1f89f4ca5
Tavi <secret9179@gmail.com>
2022-05-31 23:29:21 +0900

    feat: addRouter ViewProduct

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/Home/HomePage";
 import Test1 from "./pages/Test1/Test1";
+import ProductList from "./components/ViewProduct/ProductList";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/test1" element={<Test1 />} />
-          <Route path="/test2" />
+          <Route path="/ProductList" element={<ProductList />} />
           <Route path="/test3" />
           <Route path="/test4" />
           <Route path="/test5" />
