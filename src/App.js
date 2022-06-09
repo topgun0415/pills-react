ae6eabddcc2d56a2fd69c961095d8708215dab09
philipuuu <topgun0415@naver.com>
2022-06-09 23:33:38 +0900

    Feat : Reviews pages routing connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 /** @format */
 
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import HomePage from "./pages/Home/HomePage";
-import Test1 from "./pages/Test1/Test1";
-import ProductList from "./components/ViewProduct/ProductList";
+import HomePage from './pages/Home/HomePage';
+import ProductList from './components/ViewProduct/ProductList';
+import Reviews from './pages/Reviews/Reviews';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/test1" element={<Test1 />} />
-          <Route path="/ProductList" element={<ProductList />} />
-          <Route path="/test3" />
-          <Route path="/test4" />
-          <Route path="/test5" />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/Reviews' element={<Reviews />} />
+          <Route path='/ProductList' element={<ProductList />} />
+          <Route path='/test3' />
+          <Route path='/test4' />
+          <Route path='/test5' />
         </Routes>
       </div>
     </BrowserRouter>
